de08e4f0e18c9440db2adca20af1bc4b93b77f3c
floggers <enemyfloggers@163.com>
2019-11-10 10:48:17 +0800

    提交代码

---
+++ CPP_DS_1/CPP_DS_1/test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string Change_Str(string &src) {
+	string str2;
+	str2.reserve(101);
+	string::iterator its = src.begin();
+	string::iterator its2 = str2.begin();
+	for (its;its != src.end();++its) {
+		str2 += *its;
+	}
+	for (its;its != src.end();++its) {
+	int pos1=	str2.rfind(*its);
+		int pos2=str2.find(*its);
+		if (pos1 != pos2) {
+			str2.erase(pos1, 1);
+			
+		}
+		else if(pos1==pos2){
+			continue;
+		}
+	}
+	return str2;
+}
+
+int main() {
+	string str1;
+	str1.reserve(101);
+	getline(cin, str1);
+	cout << Change_Str(str1);
+	system("pause");
+	return 0;
+}
