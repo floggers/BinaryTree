8ee678453dd47573795435b9e07cca23715031fd
floggers <enemyfloggers@163.com>
2019-11-14 11:18:30 +0800

    提交代码

--- 每日一题day_08/每日一题day——08/test.cpp
+++ 每日一题day_08/每日一题day——08/test.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-#if 1
+#if 0
 
 int Func(int a, int b) {
 	int outc = 0;
@@ -26,3 +29,65 @@ int main() {
 }
 
 #endif
+
+#if 1
+
+int which_Sort_case(vector<string> vs) {
+	int flag = 0;
+	vector<string> vs_buf(vs);      
+	vector<int> vi;
+	vector<int> vi_buf;
+	for (int i = 0;i < vs.size();i++) {
+		vi[i] = vs[i].size();
+	}
+	sort(vs_buf.begin(), vs_buf.end());       //×ÖµäÐòÅÅÐò
+	sort(vi_buf.begin(), vi_buf.end());      //³¤¶ÈÅÅÐò
+	for (int i = 0;i < vs.size();i++) {
+		if (vs[i] != vs_buf[i]) {
+			if (vs[i].size() == vi_buf[i]) {
+				flag = 2;  //°´ÕÕ³¤¶ÈÅÅÐò   lengths
+			}
+			else {
+				flag = 4;  //  none
+			}
+		}
+			if (vs[i].size() != vi_buf[i]) {
+				if (vs[i] == vs_buf[i]) {
+					flag = 1;
+				}
+				else {
+					flag = 4;
+				}
+			}
+			if (vs[i] == vs_buf[i] && vs[i].size() == vi_buf[i]) {
+				flag = 3;
+			}
+	}
+	return flag;
+}
+
+int main() {
+	int swh;
+	vector<string> vs;
+	int n;
+	cin >> n;
+	vs.resize(n);
+	for (int i = 0;i < n;i++) {
+		cin.get();
+		getline(cin, vs[i]);
+	}
+	swh = which_Sort_case(vs);
+	switch (swh)
+	{
+	case 1:cout << "lexicographically" << endl;
+	case 2:cout << "lengths" << endl;
+	case 3:cout << "both" << endl;
+	case 4:cout << "none" << endl;
+	default:cout << "ÊäÈë´íÎó£¡" << endl;
+		break;
+	}
+	system("pause");
+	return 0;
+}
+
+#endif 
