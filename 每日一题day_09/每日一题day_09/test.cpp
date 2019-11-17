e9d6cb797d84896e419d16cd0889e8d231e5bfe0
floggers <enemyfloggers@163.com>
2019-11-17 13:10:23 +0800

    更新代码

--- 每日一题day_09/每日一题day_09/test.cpp
+++ 每日一题day_09/每日一题day_09/test.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-#if 1
+#if 0
 
 class Solution {
 public:
@@ -29,4 +30,34 @@ public:
 		return 0;
 	}
 
+#endif
+
+
+#if 1
+
+	int Total_ways(int n, int m) {
+		int ways = 0;
+		vector<vector<int>> vi;
+		vi.resize(n+1);
+		for (int i = 0;i <=n;i++) {
+			vi[i].resize(m+1,1);
+		}
+		for (int i = 0;i <= n;i++) {
+			for (int j = 0;j <= m;j++) {
+				if (j - 1 >= 0 && i - 1 >= 0) {
+					vi[i][j] = vi[i][j - 1] + vi[i - 1][j];
+				}
+			}
+		}
+		ways = vi[n][m];
+		return ways;
+	}
+
+	int main() {
+		int a, b;
+		cin >> a >> b;
+		cout << Total_ways(a, b) << endl;
+		system("pause");
+		return 0;
+	}
 #endif
