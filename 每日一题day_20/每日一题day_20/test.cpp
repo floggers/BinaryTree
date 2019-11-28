cb746b07de952730312f5397b6adb6e0b69321c7
floggers <enemyfloggers@163.com>
2019-11-28 10:07:55 +0800

    反转字符串 最长公共子串

--- 每日一题day_20/每日一题day_20/test.cpp
+++ 每日一题day_20/每日一题day_20/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 #if 0
@@ -76,10 +77,36 @@ int main() {
 
 #if 1
 
-
+int maxLong_Child_str(string str1, string str2) {
+	vector<vector<int>> table(str1.size(), vector<int>(str2.size(),0));
+	int maxLen = 0;
+	//int maxEnd = 0;
+	for (int i = 0;i < str1.size() ;i++) {
+		for (int j = 0;j < str2.size() ;j++) {
+			if (str1[i] == str2[j]) {
+				if (i == 0 || j == 0) {
+					table[i][j] = 1;
+				}
+				else {
+					table[i][j] = table[i - 1][j - 1] + 1;
+				}
+			}
+			if (table[i][j] > maxLen) {
+				maxLen = table[i][j];
+				//maxEnd = i;              //若记录i,则最后获取的是str1的子串
+			}
+		}
+	}
+	//return str1.substr(maxEnd - maxLen + 1, maxLen);    //substr(起始位置,切割长度)
+	return maxLen;
+}
 
 int main() {
-
+	string str1, str2;
+	int buf;
+	cin >> str1 >> str2;
+	buf = maxLong_Child_str(str1, str2);
+	cout << buf << endl;
 	system("pause");
 	return 0;
 }
