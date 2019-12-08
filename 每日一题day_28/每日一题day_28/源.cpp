57bd6d8933e08d624ec7e1a7e632e4a8c9a6b444
floggers <enemyfloggers@163.com>
2019-12-08 14:39:37 +0800

    奇位偶数 猴子分桃

---
+++ 每日一题day_28/每日一题day_28/源.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+#if 1
+
+int main() {
+	int n;
+	while (cin >> n) {
+		if (n == 0) break;
+		long total = pow(5, n) - 4;
+		long left = pow(4, n) + n - 4;
+		cout << total << " " << left << endl;
+	}
+	system("pause");
+	return 0;
+}
+
+#endif
