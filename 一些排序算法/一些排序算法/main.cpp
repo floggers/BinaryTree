6457a726d2edda5126a1de9d0c00980b8dbf0ef9
floggers <enemyfloggers@163.com>
2020-07-19 21:44:19 +0800

    push

--- 一些排序算法/一些排序算法/main.cpp
+++ 一些排序算法/一些排序算法/main.cpp
@@ -30,12 +30,35 @@ void QuickSort(int* arr, int left, int right) {
 
 /******************************************************** 堆  排 *******************************************************/
 
+//建一个大根堆
+void MakeHeap(int* arr, int end) {  //end表示最后一个元素下标
+	int last = (end - 1) / 2;  //最后一个非叶子节点的下标
+	for (int i = last;i >= 0;--i) {
+		int left_son = 2 * i + 1;    //左孩子下标
+		int right_son = 2 * i + 2;   //右孩子下标
+		int bigson = left_son;       //孩子中值较大的一个下标
+		if (right_son <= end&&arr[right_son] > arr[left_son]) {
+			bigson = right_son;
+		}
+		if (arr[bigson] > arr[i]) {
+			swap(arr[bigson], arr[i]);
+		}
+	}
+}
+
+void HeapSort(int*arr, int num) {  //num表示元素总数
+	for (int i = num - 1;i > 0;--i) {
+		MakeHeap(arr, i);
+		swap(arr[i], arr[0]);
+	}
+}
 
 /******************************************************** 归  并 *******************************************************/
 
 int main() {
 	int arr[] = { 1,2,0,1,2,1,1,1 };
-	QuickSort(arr, 0, 7);
+	//QuickSort(arr, 0, 7);
+	HeapSort(arr, 8);
 	for (auto &e : arr) {
 		cout << e << " ";
 	}
