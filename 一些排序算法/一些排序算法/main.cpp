19897fa2c39aa7ea6edba1dd77a8a7cc48254fc9
floggers <enemyfloggers@163.com>
2020-07-25 21:45:11 +0800

    push

--- 一些排序算法/一些排序算法/main.cpp
+++ 一些排序算法/一些排序算法/main.cpp
@@ -55,10 +55,28 @@ void HeapSort(int*arr, int num) {  //num表示元素总数
 
 /******************************************************** 归  并 *******************************************************/
 
+/********************************************************选择排序*******************************************************/
+
+void SelectSort(int* arr, int size) {
+	for (int i = 0;i < size - 1;++i) {
+		int Min = i;
+		for (int j = i + 1;j < size;++j) {
+			if (arr[j] < arr[Min]) {
+				Min = j;
+			}
+		}
+		if (Min != i) {
+			swap(arr[i], arr[Min]);
+		}
+	}
+}
+
+
 int main() {
-	int arr[] = { 1,1,1,1,1,1,1,1 };
+	int arr[] = { 1,9,1,2,4,0,1,1 };
 	//QuickSort(arr, 0, 7);
-	HeapSort(arr, 8);
+	//HeapSort(arr, 8);
+	SelectSort(arr, 8);
 	for (auto &e : arr) {
 		cout << e << " ";
 	}
