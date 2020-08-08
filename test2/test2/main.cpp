6062f2b11371eeb179070289a74e057cbd4bda5e
floggers <enemyfloggers@163.com>
2020-08-08 18:05:02 +0800

    push

--- test2/test2/main.cpp
+++ test2/test2/main.cpp
@@ -1,22 +1,70 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-typedef union{
-	int a;
-	char c;
-}A;
 
 int main(){
-	A myunion;
-	myunion.a = 1;
-	if (myunion.c == 1)         //根据ch的值判断系统大小端
-	{
-		printf("系统为小端存储!\n");
+#if 1
+	vector<int> star;
+	int n;
+	for (int i = 0;i < 5;++i) {
+		cin >> n;
+		star.push_back(n);
 	}
-	else
-	{
-		printf("系统为大端存储!\n");
+	int stars = 0;
+	int sum = 0;
+	for (int i = 1;i <= 5;++i) {
+		stars += star[i - 1] * i;
+		sum += star[i - 1];
 	}
+	float avr;
+	if (sum != 0) {
+		avr = stars / (sum * 1.0);
+		string str = to_string(avr);
+		//printf("%0.2f\n", avr);
+		for (int i = 0;i < 3;++i) {
+			cout << str[i];
+		}
+		cout << endl;
+	}
+	else {
+		avr = 0.0;
+		printf("%0.1f\n", avr);
+	}
+
+#endif
+
+#if 0
+	int n;
+	while (cin >> n) {
+		int money = 0;
+		vector<int> price;
+		for (int i = 0;i < n;++i) {
+			int a;
+			cin >> a;
+			price.push_back(a);
+		}
+		for (int i = 0;i < price.size();++i) {
+			if (i + 1 < price.size() && price[i] == price[i + 1]) {
+				++money;
+				int tmp = price[i] + 1;
+				price[i] = tmp;
+				price.erase(price.begin() + i + 1);
+				--i;
+			}
+			else if (i - 1 >= 0 && price[i] == price[i - 1]) {
+				++money;
+				int tmp = price[i] + 1;
+				price[i - 1] = tmp;
+				price.erase(price.begin() + i);
+				--i;
+			}
+		}
+		cout << money << endl;
+	}
+#endif
 system("pause");
 return 0;
 }
