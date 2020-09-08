078614dfbe6e554cb10d4d7b288115fb7522ea66
floggers <enemyfloggers@163.com>
2020-09-08 10:57:01 +0800

    string类的实现

--- MyString/MyString/main.cpp
+++ MyString/MyString/main.cpp
@@ -30,25 +30,78 @@ public:
 		}
 	}
 
+	String operator+(const String& s) {
+		char* p = new char(strlen(_str) + strlen(s._str) + 1);
+		strcpy(p, _str);
+		strcat(p, s._str);
+		String tmp(p);
+		delete[] p;
+		return tmp;
+	}
+
+	String& operator+=(const String& s) {
+		char* p = new char(strlen(_str) + strlen(s._str) + 1);
+		strcpy(p, _str);
+		strcat(p, s._str);
+		delete[] _str;
+		_str = p;
+		return *this;
+	}
+
+	bool operator==(const String& s) {
+		return strcmp(_str, s._str) == 0;
+	}
+
+	bool operator!=(const String& s) {
+		return strcmp(_str, s._str) != 0;
+	}
+
+	bool operator>=(const String& s) {
+		return strcmp(_str, s._str) >= 0;
+	}
+
+	bool operator<=(const String& s) {
+		return strcmp(_str, s._str) <= 0;
+	}
+
+	bool operator>(const String& s) {
+		return strcmp(_str, s._str) > 0;
+	}
+
+	bool operator<(const String& s) {
+		return strcmp(_str, s._str) < 0;
+	}
+
+	char& operator[](int index) {
+		return _str[index];
+	}
+
 	friend ostream& operator<<(ostream& os, const String& s) {
 		os << s._str;
 		return os;
 	}
 
+	friend iostream& operator>>(iostream& is, String& s) {
+		is >> s._str;
+		return is;
+	}
+
 	~String() {
 		if (_str) {
 			delete[] _str;
+			_str = NULL;
 		}
 	}
 };
 
 int main() {
-	String s1("Ìì°Ô¶¯°Ôchua£¡");
+	String s1("chua£¡");
 	String s2(s1);
 	String s3 = s2;
 	cout << s1.get_str() << endl;
 	cout << s2.get_str() << endl;
 	cout << s3 << endl;
+	cout << s3[1] << endl;
 	system("pause");
 	return 0;
 }
